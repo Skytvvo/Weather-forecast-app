5fe4af6a2b7d2e5dcf24ee223b6dbaf2a5d5cbcd
Ilya Kazakou <pouutewqer@gmail.com>
2021-03-15 15:42:10 +0300

    Port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const config = require("./Config/default.json");
 
 
-const PORT =  process.env.PORT || 9999;
+const PORT =  config.port || 9999;
 const mongoose = require("mongoose");
 
 const cors = require('cors');
