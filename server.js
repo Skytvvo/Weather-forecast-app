4b137b3710822837f8d41363156ac7967918fea1
Ilya Kazakou <pouutewqer@gmail.com>
2021-03-15 17:14:05 +0300

    Try again

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+
 const config = require("./Config/default.json");
 
 
@@ -7,7 +8,9 @@ const PORT =  process.env.PORT || 9999;
 const mongoose = require("mongoose");
 
 const cors = require('cors');
+const path = require("path");
 app.options("*",cors());
+app.use(express.static(path.join(__dirname,"/build")))
 
 app.use(express.json());
 
